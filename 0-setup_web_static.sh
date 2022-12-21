33f0bc818a382635e4023eff41a8497ad8d38b2c
kwabenasapong <kwabenasapong@gmail.com>
2022-12-21 22:54:01 +0000

    Add task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
-#Creation of files and others for web static
-sudo apt-get update -y
-sudo apt-get install nginx -y
-sudo mkdir data/web_static/releases/test/
-sudo mkdir data/web_static/shared/
-sudo echo "<html>
-         <head>
-         </head>
-         <body>
-             Holberton School
-         </body>
-      </html>" > /data/web_static/releases/test/index.html
-sudo ln -nsf  /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enable/default 
+# configuring my servers as web servers
+sudo apt update -y
+sudo apt install nginx -y
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "it works" | sudo tee /data/web_static/releases/test/index.html
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+ln -s /data/web_static/releases/test/ /data/web_static/current
+replace="server_name _;"
+new="server_name kwabenasapong.tech;\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
+sudo sed -i "s/$replace/$new/" /etc/nginx/sites-enabled/default
 sudo service nginx restart
