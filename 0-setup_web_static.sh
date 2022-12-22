01a30a1554b4d7bdc755d23f2120b06d5bb274f5
kwabenasapong <kwabenasapong@gmail.com>
2022-12-22 09:23:16 +0000

    Update task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
-# Write a Bash script that sets up your web servers for the deployment of web_static
+# configuring my servers as web servers
 
-#install nginx
-sudo apt-get update -y
-sudo apt-get install nginx -y
+# Update and install nginx
+sudo apt update -y
+sudo apt install nginx -y
 
-#create neccessary folders
-sudo mkdir -p '/data/web_static/shared/'
-sudo mkdir -p '/data/web_static/releases/test'
-sudo touch '/data/web_static/releases/test/index.html'
+# Create necessary directories
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-# index.html message
-sudo echo "Welcome, this is $(hostname) server" |sudo tee /data/web_static/releases/test/index.html
+# Create index.html
+echo "it works" | sudo tee /data/web_static/releases/test/index.html
 
-# creates symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# ownership
+# Change ownership
 sudo chown -hR ubuntu:ubuntu /data/
 
-# configuration to serve the content
-sudo sed -i "59i\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
+# Create symlinks
+rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo rm -rf /etc/nginx/sites-enabled
+sudo ln -sf /etc/nginx/sites-available/ /etc/nginx/sites-enabled
 
-# restart nginx server
+# configure Nginx Servers
+replace="server_name _;"
+new="server_name _;\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
+sudo sed -i "s/$replace/$new/" /etc/nginx/sites-enabled/default
+
+# Restart Nginx
 sudo service nginx restart
