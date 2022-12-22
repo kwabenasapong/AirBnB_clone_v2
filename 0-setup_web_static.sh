b56f1152d643313cb1835ffad6d3084d775b0e71
kwabenasapong <kwabenasapong@gmail.com>
2022-12-22 08:30:16 +0000

    Update task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,8 @@ echo "it works" | sudo tee /data/web_static/releases/test/index.html
 sudo chown -hR ubuntu /data/
 sudo chgrp -hR ubuntu /data/
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo rm -rf /etc/nginx/sites-enabled
+sudo ln -sf /etc/nginx/sites-available/ /etc/nginx/sites-enabled
 replace="server_name _;"
 new="server_name kwabenasapong.tech;\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
 sudo sed -i "s/$replace/$new/" /etc/nginx/sites-enabled/default
