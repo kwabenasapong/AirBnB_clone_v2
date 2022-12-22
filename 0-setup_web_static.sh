c38f9f40c26cdbb95090e9e02b05223e9cec5e00
kwabenasapong <kwabenasapong@gmail.com>
2022-12-22 09:11:59 +0000

    Update task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,26 @@
 #!/usr/bin/env bash
-# configuring my servers as web servers
+# Write a Bash script that sets up your web servers for the deployment of web_static
 
-# Update and install nginx
-sudo apt update -y
-sudo apt install nginx -y
+#install nginx
+sudo apt-get update -y
+sudo apt-get install nginx -y
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+#create neccessary folders
+sudo mkdir -p '/data/web_static/shared/'
+sudo mkdir -p '/data/web_static/releases/test'
+sudo touch '/data/web_static/releases/test/index.html'
 
-# Create index.html
-echo "it works" | sudo tee /data/web_static/releases/test/index.html
+# index.html message
+sudo echo "Welcome, this is $(hostname) server" |sudo tee /data/web_static/releases/test/index.html
 
-# Change ownership
+# creates symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# ownership
 sudo chown -hR ubuntu:ubuntu /data/
 
-# Create symlinks
-rm -rf /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo rm -rf /etc/nginx/sites-enabled
-sudo ln -sf /etc/nginx/sites-available/ /etc/nginx/sites-enabled
+# configuration to serve the content
+sudo sed -i "59i\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 
-# configure Nginx Servers
-replace="server_name _;"
-new="server_name kwabenasapong.tech;\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
-sudo sed -i "s/$replace/$new/" /etc/nginx/sites-enabled/default
-
-# Restart Nginx
+# restart nginx server
 sudo service nginx restart
---
+++ 01-setup_web_static.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# configuring my servers as web servers
+
+# Update and install nginx
+sudo apt update -y
+sudo apt install nginx -y
+
+# Create necessary directories
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+# Create index.html
+echo "it works" | sudo tee /data/web_static/releases/test/index.html
+
+# Change ownership
+sudo chown -hR ubuntu:ubuntu /data/
+
+# Create symlinks
+rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo rm -rf /etc/nginx/sites-enabled
+sudo ln -sf /etc/nginx/sites-available/ /etc/nginx/sites-enabled
+
+# configure Nginx Servers
+replace="server_name _;"
+new="server_name kwabenasapong.tech;\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
+sudo sed -i "s/$replace/$new/" /etc/nginx/sites-enabled/default
+
+# Restart Nginx
+sudo service nginx restart
