4258d3f3eb2e89d59532809d390dbda93425d627
kwabenasapong <kwabenasapong@gmail.com>
2022-12-22 09:27:32 +0000

    Update task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,18 +8,19 @@ sudo apt install nginx -y
 # Create necessary directories
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+sudo touch '/data/web_static/releases/test/index.html'
 
 # Create index.html
 echo "it works" | sudo tee /data/web_static/releases/test/index.html
 
-# Change ownership
-sudo chown -hR ubuntu:ubuntu /data/
-
 # Create symlinks
-rm -rf /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo rm -rf /etc/nginx/sites-enabled
 sudo ln -sf /etc/nginx/sites-available/ /etc/nginx/sites-enabled
+
+# Change ownership
+sudo chown -hR ubuntu:ubuntu /data/
 
 # configure Nginx Servers
 replace="server_name _;"
