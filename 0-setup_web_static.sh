db9838a05e29c5d140795429f7be3defa81ba659
kwabenasapong <kwabenasapong@gmail.com>
2022-12-28 09:49:02 +0000

    Update task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,26 @@
 #!/usr/bin/env bash
-# configuring my servers as web servers
+# Write a Bash script that sets up your web servers for the deployment of web_static
 
-# Update and install nginx
+#install nginx
 sudo apt-get update -y
 sudo apt-get install nginx -y
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+#create neccessary folders
+sudo mkdir -p '/data/web_static/shared/'
+sudo mkdir -p '/data/web_static/releases/test'
+sudo touch '/data/web_static/releases/test/index.html'
 
-# Create index.html
-sudo echo "This is $(hostname) an ALX student server" | sudo tee /data/web_static/releases/test/index.html
+# index.html message
+sudo echo "Welcome, this is $(hostname) server" |sudo tee /data/web_static/releases/test/index.html
 
-# Create symlinks
-sudo rm -rf /data/web_static/current
+# creates symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo rm -rf /etc/nginx/sites-enabled
-sudo ln -sf /etc/nginx/sites-available/ /etc/nginx/sites-enabled
 
-# Change ownership
+# ownership
 sudo chown -hR ubuntu:ubuntu /data/
 
-# configure Nginx Servers
-replace="server_name _;"
-new="server_name _;\n\n\tlocation \/hbnb_static \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
-sudo sed -i "s/$replace/$new/" /etc/nginx/sites-enabled/default
+# configuration to serve the content
+sudo sed -i "59i\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 
-# Restart Nginx
+# restart nginx server
 sudo service nginx restart
