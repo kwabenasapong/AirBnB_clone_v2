b643737cb054ee3a33bf0d5c48a620665a176725
kwabenasapong <kwabenasapong@gmail.com>
2023-01-17 14:20:42 +0000

    Add task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,15 +8,19 @@ from fabric.api import *
 
 env.hosts = ['34.232.68.81', '54.198.29.36']
 
+
 def do_deploy(archive_path):
     """
     Uploads the archive to the /tmp/ directory of the web server,
-    uncompress the archive to the folder /data/web_static/releases/<archive filename without extension> on the web server,
+    uncompress the archive to the folder /data/web_static/releases/<archive
+    filename without extension> on the web server,
     deletes the archive from the web server,
     deletes the symbolic link /data/web_static/current from the web server,
-    creates a new symbolic link /data/web_static/current on the web server, linked to the new version of your code.
+    creates a new symbolic link /data/web_static/current on the
+    web server, linked to the new version of your code.
     Returns:
-        bool: True if all operations have been done correctly, otherwise returns False
+        bool: True if all operations have been done correctly,
+        otherwise returns False
     """
     if not os.path.isfile(archive_path):
         return False
